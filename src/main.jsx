1e26db8fef01c01917d93206ae43a32ad8643b65
AdminPM01 <admin@promosie.de>
2026-05-28 16:50:12 +0200

    Change import from change_order_validator to app

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './change_order_validator.jsx'
+import App from './app.jsx'
  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
